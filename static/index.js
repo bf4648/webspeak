2cebf2c230bb1732ddb153fe782f124e90aba485
Benjamin Frazier <benjamin.frazier@gmail.com>
2017-07-25 16:10:45 -0400

    can not manipulate options for the voices

--- static/index.js
+++ static/index.js
@@ -4,10 +4,10 @@
 var supportMsg = document.getElementById('msg');
 
 if ('speechSynthesis' in window) {
-	supportMsg.innerHTML = 'Your browser <strong>supports</strong> speech synthesis.';
+    supportMsg.innerHTML = 'Your browser <strong>supports</strong> speech synthesis.';
 } else {
-	supportMsg.innerHTML = 'Sorry your browser <strong>does not support</strong> speech synthesis.<br>Try this in <a href="https://www.google.co.uk/intl/en/chrome/browser/canary.html">Chrome Canary</a>.';
-	supportMsg.classList.add('not-supported');
+    supportMsg.innerHTML = 'Sorry your browser <strong>does not support</strong> speech synthesis.<br>Try this in <a href="https://www.google.co.uk/intl/en/chrome/browser/canary.html">Chrome Canary</a>.';
+    supportMsg.classList.add('not-supported');
 }
 
 
@@ -28,21 +28,21 @@ var pitchInput = document.getElementById('pitch');
 
 // Fetch the list of voices and populate the voice options.
 function loadVoices() {
-  // Fetch the available voices.
-	var voices = speechSynthesis.getVoices();
+    // Fetch the available voices.
+    var voices = speechSynthesis.getVoices();
 
-  // Loop through each of the voices.
-	voices.forEach(function(voice, i) {
-    // Create a new option element.
-		var option = document.createElement('option');
+    // Loop through each of the voices.
+    voices.forEach(function(voice, i) {
+        // Create a new option element.
+        var option = document.createElement('option');
 
-    // Set the options value and text.
-		option.value = voice.name;
-		option.innerHTML = voice.name;
+        // Set the options value and text.
+        option.value = voice.name;
+        option.innerHTML = voice.name;
 
-    // Add the option to the voice selector.
-		voiceSelect.appendChild(option);
-	});
+        // Add the option to the voice selector.
+        voiceSelect.appendChild(option);
+    });
 }
 
 // Execute loadVoices.
@@ -50,38 +50,60 @@ loadVoices();
 
 // Chrome loads voices asynchronously.
 window.speechSynthesis.onvoiceschanged = function(e) {
-  loadVoices();
+    loadVoices();
 };
 
+function reconnect() {
+    setTimeout(connect, 5000);
+}
+
+function status(status) {
+    var statuselement = document.getElementById('speech-msg');
+    statuselement.value = status;
+}
+
+function connect() {
+    websock = new WebSocket("ws://" + window.location.host);
+    websock.onopen = function(event) {
+        status('Waiting for first request');
+    }
+
+    websock.onmessage = function(event) {
+        var data = JSON.parse(event.data);
+        speak(data['speak']);
+    }
+}
 
 // Create a new utterance for the specified text and add it to
 // the queue.
 function speak(text) {
-  // Create a new instance of SpeechSynthesisUtterance.
-	var msg = new SpeechSynthesisUtterance();
+    // Create a new instance of SpeechSynthesisUtterance.
+    var msg = new SpeechSynthesisUtterance();
 
-  // Set the text.
-	msg.text = text;
+    // Set the text.
+    msg.text = text;
 
-  // Set the attributes.
-	msg.volume = parseFloat(volumeInput.value);
-	msg.rate = parseFloat(rateInput.value);
-	msg.pitch = parseFloat(pitchInput.value);
+    // Set the attributes.
+    msg.volume = parseFloat(volumeInput.value);
+    msg.rate = parseFloat(rateInput.value);
+    msg.pitch = parseFloat(pitchInput.value);
 
-  // If a voice has been selected, find the voice and set the
-  // utterance instance's voice attribute.
-	if (voiceSelect.value) {
-		msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelect.value; })[0];
-	}
+    // If a voice has been selected, find the voice and set the
+    // utterance instance's voice attribute.
+    if (voiceSelect.value) {
+        msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelect.value; })[0];
+    }
 
-  // Queue this utterance.
-	window.speechSynthesis.speak(msg);
+    // Queue this utterance.
+    window.speechSynthesis.speak(msg);
 }
 
 
 // Set up an event listener for when the 'speak' button is clicked.
 button.addEventListener('click', function(e) {
-	if (speechMsgInput.value.length > 0) {
-		speak(speechMsgInput.value);
-	}
+    if (speechMsgInput.value.length > 0) {
+        speak(speechMsgInput.value);
+    }
 });
+
+connect();
