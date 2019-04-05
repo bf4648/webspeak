01f68fe6fb064f94d4290bd37c6e3aefe95c8605
Benjmain Frazier <benjamin.frazier@gmail.com>
2019-04-05 01:03:30 -0400

    cleaned up setup-pyenv.sh script

--- scripts/setup-pyenv.sh
+++ scripts/setup-pyenv.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-pyenv install -s 3.4.0
-pyenv virtualenv 3.4.0 web-speak-3.4.0
-pyenv activate web-speak-3.4.0
-pyenv rehash
+install_pyenv() {
+	pyenv install -s 3.4.0
+	pyenv virtualenv 3.4.0 web-speak-3.4.0
+	pyenv activate web-speak-3.4.0
+	pyenv rehash
+}
+
+main() {
+	install_pyenv
+}
